6aa6e43c5b39b08dffa0c205873bb6b4b7470ecc
Jitesh Adesara <34264491+Jitesh-21@users.noreply.github.com>
2020-03-15 23:17:37 +0530

    Update HelloClass.java

--- src/javaProgramming/HelloClass.java
+++ src/javaProgramming/HelloClass.java
@@ -6,12 +6,7 @@ public class HelloClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World"
 				+ " ");
-		System.out.println("Hello World"
-				+ " ");
-		System.out.println("Hello World"
-				+ " ");
-		System.out.println("Hello World"
-				+ " ");
+		
 	}
 
 }
