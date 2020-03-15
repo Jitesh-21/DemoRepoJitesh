ea98cd65b47b03da68a1c539b8bd604abad86356
Jitesh_21 <jitesh.adesara218@gmail.com>
2020-03-15 18:09:01 +0530

    Rajeev is trying to push code in Jitesh's Repo

---
+++ src/javaProgramming/NewClass.java
@@ -0,0 +1,12 @@
+package javaProgramming;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is for testing and git testing ");
+
+	}
+
+}
